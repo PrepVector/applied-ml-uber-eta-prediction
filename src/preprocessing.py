07f95bcc66c296eeae7fe166d96ae8e6db2d97a8
Shrimanta Satpati <82357659+shrimantasatpati@users.noreply.github.com>
2024-06-14 21:31:21 +0530

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,23 +1,25 @@
 import numpy as np
 import pandas as pd
 import pickle
+import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-import xgboost as xgb
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
 class DataProcessing:
     def __init__(self):
         # The earth's radius (in km)
-        self.R = 6371  
+        self.R = 6371
+        
 
     def update_column_name(self, df):
         """
         Renames the 'Weatherconditions' column to 'Weather_conditions' in the given DataFrame df.
         """
+        
         df.rename(columns={'Weatherconditions': 'Weather_conditions'}, inplace=True)
-
+        
 
     def extract_feature_value(self, df):
         """
@@ -39,6 +41,7 @@ class DataProcessing:
         """
         Extracts the label value (Time_taken(min)) from the 'Time_taken(min)' column in the given DataFrame df.
         """
+        
         df['Time_taken(min)'] = df['Time_taken(min)'].apply(lambda x: int(x.split(' ')[1].strip()))
 
 
@@ -65,6 +68,7 @@ class DataProcessing:
         """
         Converts the string 'NaN' to a float NaN value in the given DataFrame df.
         """
+        
         df.replace('NaN', float(np.nan), regex=True, inplace=True)
 
 
@@ -132,7 +136,9 @@ class DataProcessing:
         """
         Converts degrees to radians.
         """
+        
         return degrees * (np.pi/180)
+        
 
     def distcalculate(self, lat1, lon1, lat2, lon2):
         """
@@ -159,11 +165,11 @@ class DataProcessing:
         df['distance'] = np.nan
 
         for i in range(len(df)):
-            df.loc[i, 'distance'] = self.distcalculate(df.loc[i, 'Restaurant_latitude'],
+            df.loc[i, "distance"] = self.distcalculate(df.loc[i, 'Restaurant_latitude'],
                                                     df.loc[i, 'Restaurant_longitude'],
                                                     df.loc[i, 'Delivery_location_latitude'],
                                                     df.loc[i, 'Delivery_location_longitude'])
-        df.distance = df.distance.astype("int64")
+        df["distance"] = df["distance"].astype("int64")
 
 
     def label_encoding(self, df):
@@ -207,9 +213,7 @@ class DataProcessing:
         """
         
         scaler = StandardScaler()
-
         scaler.fit(X_train)
-
         X_train = scaler.transform(X_train)
         X_test = scaler.transform(X_test)
         return X_train, X_test, scaler
@@ -227,7 +231,6 @@ class DataProcessing:
         self.extract_date_features(df)
         self.calculate_time_diff(df)
         self.calculate_distance(df)
-
 
     def evaluate_model(self, y_test, y_pred):
         mae = mean_absolute_error(y_test, y_pred)
